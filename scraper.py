c405efb72299d24275bfa0609130c92ea515343d
EricSchles <ericschles@gmail.com>
2015-01-21 08:37:18 -0800

    adding link grab

--- scraper.py
+++ scraper.py
@@ -10,4 +10,8 @@ domains = pickle.load(open(".sites_to_scrape","r"))
 rs = (grequests.get(u) for u in domains)
 results = grequests.map(rs)
 for r in results:
+    html = r.text.decode("ascii","ignore")
+    obj = lxml.html.fromstring(html)
+    #gets all the hyper links
+    links = [elem for elem in obj.xpath("//a/@href")] 
     
