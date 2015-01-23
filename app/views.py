7bcd48b5e9b56fba8d2ff65f24b6afb3ec7482ea
EricSchles <ericschles@gmail.com>
2015-01-23 06:38:12 -0800

    works, making it funcitonal

--- app/views.py
+++ app/views.py
@@ -1,20 +1,25 @@
+from app import app
+import datetime
+import os
+from flask import render_template, request, redirect
+
 
 @app.route("/index",methods=["GET","POST"])
 @app.route("/",methods=["GET","POST"])
 def index():
-	return render_template("index.html",show_results=False)
+	return render_template("index.html")#,show_results=False)
 
 @app.route("/Scraper",methods=["GET","POST"])
 def scraping():
         s = Scraper(testing=True)
         ads = s.get_ads()
         #print ads
-        for ad in ads:
-               ad = Ads(ad)
-               db.session.add(ad)
-               db.session.commit()
-        return render_template("/index",show_results=True)
+        # for ad in ads:
+        #        ad = Ads(ad)
+        #        db.session.add(ad)
+        #        db.session.commit()
+        return render_template("/index")#,show_results=True)
 
 @app.route("/AdResults",methods=["GET","POST"])
 def ad_results():
-        return render_template("results.html",ads=Ads.query.all())
+        return render_template("results.html")#,ads=Ads.query.all())
